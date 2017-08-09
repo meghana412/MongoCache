fd536f05ce2b290e224d2c6477e9a29e310124ef
meghana412 <kagita12@gmail.com>
2017-08-08 22:11:16 -0500

    Add files via upload

---
+++ MongoDBCache/src/main/java/com/meghana/Book.java
@@ -0,0 +1,24 @@
+package com.meghana;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import lombok.Data;
+
+@Data
+
+public class Book {
+	
+    @Id
+
+    private String id;
+
+    @Indexed
+
+    private String title;
+
+    private String author;
+
+    private String description;
+
+}
---
+++ MongoDBCache/src/main/java/com/meghana/BookRepository.java
@@ -0,0 +1,15 @@
+package com.meghana;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookRepository extends MongoRepository<Book, String>
+
+{
+
+    Book findByTitle(String title);
+
+    void delete(String title);
+
+
+
+}
